8286bf5a510a5b2c027f20e0d598fbc9b9ec6bbc
Justin Ribeiro <justin@justinribeiro.com>
2014-10-27 14:49:28 -0700

    Add CC, R-CC, and response APDU to HCE handler

--- app/src/main/java/com/justinribeiro/demo/apps/hostcardemulation/myHostApduService.java
+++ app/src/main/java/com/justinribeiro/demo/apps/hostcardemulation/myHostApduService.java
@@ -19,13 +19,43 @@ public class myHostApduService extends HostApduService {
     // Ala... <aid-filter android:name="F0394148148100" />
     //
     private static final byte[] APDU_SELECT = {
+        (byte)0x00, // CLA	- Class - Class of instruction
+        (byte)0xA4, // INS	- Instruction - Instruction code
+        (byte)0x04, // P1	- Parameter 1 - Instruction parameter 1
+        (byte)0x00, // P2	- Parameter 2 - Instruction parameter 2
+        (byte)0x07, // Lc field	- Number of bytes present in the data field of the command
+        (byte)0xF0, (byte)0x39, (byte)0x41, (byte)0x48, (byte)0x14, (byte)0x81, (byte)0x00, // Data field - String of bytes sent in the data field of the command
+        (byte)0x00  // Le field	- Maximum number of bytes expected in the data field of the response to the command
+    };
+
+    //
+    // The following definitions are based on the APDU example in libnfc:
+    // http://nfc-tools.org/index.php?title=Libnfc:APDU_example
+    //
+    // The example has been included in this project slightly modified to handle the AID in the
+    // Android documentation (the APDU_SELECT defined above)
+    //
+    private static final byte[] CAPABILITY_CONTAINER = {
             (byte)0x00, // CLA	- Class - Class of instruction
-            (byte)0xA4, // INS	- Instruction - Instruction code
-            (byte)0x04, // P1	- Parameter 1 - Instruction parameter 1
+            (byte)0xa4, // INS	- Instruction - Instruction code
+            (byte)0x00, // P1	- Parameter 1 - Instruction parameter 1
+            (byte)0x0c, // P2	- Parameter 2 - Instruction parameter 2
+            (byte)0x02, // Lc field	- Number of bytes present in the data field of the command
+            (byte)0xe1,
+            (byte)0x03  // Le field	- Maximum number of bytes expected in the data field of the response to the command
+    };
+
+    private static final byte[] READ_CAPABILITY_CONTAINER = {
+            (byte)0x00, // CLA	- Class - Class of instruction
+            (byte)0xb0, // INS	- Instruction - Instruction code
+            (byte)0x00, // P1	- Parameter 1 - Instruction parameter 1
             (byte)0x00, // P2	- Parameter 2 - Instruction parameter 2
-            (byte)0x07, // Lc field	- Number of bytes present in the data field of the command
-            (byte)0xF0, (byte)0x39, (byte)0x41, (byte)0x48, (byte)0x14, (byte)0x81, (byte)0x00,
-            (byte)0x00  // Le field	- Maximum number of bytes expected in the data field of the response to the command
+            (byte)0x0f  // Lc field	- Number of bytes present in the data field of the command
+    };
+
+    private static final byte[] A_OKAY = {
+            (byte)0x90,  // SW1	Status byte 1 - Command processing status
+            (byte)0x00   // SW2	Status byte 2 - Command processing qualifier
     };
 
     @Override
@@ -34,11 +64,23 @@ public class myHostApduService extends HostApduService {
         Log.i(TAG, "commandApdu = " + Arrays.toString(commandApdu));
 
         if (Arrays.equals(APDU_SELECT, commandApdu)) {
-            Log.i(TAG, "processCommandApdu(): Our Application Selected!");
-            return "Hello Chromebook NFC!".getBytes();
-        } else {
+            Log.i(TAG, "processCommandApdu() | APDU_SELECT triggered");
+            return A_OKAY;
+        }
+
+        if (Arrays.equals(CAPABILITY_CONTAINER, commandApdu)) {
+            Log.i(TAG, "processCommandApdu() | CAPABILITY_CONTAINER triggered");
+            return A_OKAY;
+        }
+
+        if (Arrays.equals(READ_CAPABILITY_CONTAINER, commandApdu)) {
+            Log.i(TAG, "processCommandApdu() | READ_CAPABILITY_CONTAINER triggered");
+            return A_OKAY;
+        }
+
+        else {
             Log.i(TAG, "processCommandApdu(): Our Application Received!");
-            return "Hello From Android!".getBytes();
+            return "Hello".getBytes();
         }
     }
 
