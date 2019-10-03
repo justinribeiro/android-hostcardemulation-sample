a078d90ddd59b4ba4ad7cb52ce6cc30f21780405
Justin Ribeiro <justin@justinribeiro.com>
2019-10-03 07:55:42 -0700

    feat(a10): bring project up to date for latest (#2)

--- app/src/main/java/com/justinribeiro/demo/apps/hostcardemulation/myStartActivity.java
+++ app/src/main/java/com/justinribeiro/demo/apps/hostcardemulation/myStartActivity.java
@@ -1,15 +1,15 @@
 package com.justinribeiro.demo.apps.hostcardemulation;
 
-import android.graphics.Color;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class myStartActivity extends ActionBarActivity {
+public class myStartActivity extends AppCompatActivity {
     private static final String TAG = "JDR HostCardEmulation";
 
     @Override
@@ -17,12 +17,8 @@ public class myStartActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ActionBar toolbar = getSupportActionBar();
         toolbar.setTitle(R.string.app_name);
-
-        // Android Studio + app:theme = angry for me at moment
-        // just set the text white really quick
-        toolbar.setTitleTextColor(Color.parseColor("#ffffff"));
 
         Button setNdef = (Button) findViewById(R.id.set_ndef_button);
         setNdef.setOnClickListener(new View.OnClickListener() {
